c613bb1a760c921abaef4a7b5d02e85af010dd26
Adriel Bomfim <adrielvbomfim@gmail.com>
2021-07-23 12:25:05 -0300

    feat: insert illustrative image to bottomsheet

--- lib/features/setup/presentation/widgets/incorrect_mnemonic_widget.dart
+++ lib/features/setup/presentation/widgets/incorrect_mnemonic_widget.dart
@@ -5,7 +5,49 @@ import 'package:polkadex/common/utils/styles.dart';
 import 'package:polkadex/common/utils/extensions.dart';
 import 'package:polkadex/common/configs/app_config.dart';
 
-class IncorrectMnemonicWidget extends StatelessWidget {
+class IncorrectMnemonicWidget extends StatefulWidget {
+  @override
+  _IncorrectMnemonicWidgetState createState() =>
+      _IncorrectMnemonicWidgetState();
+}
+
+class _IncorrectMnemonicWidgetState extends State<IncorrectMnemonicWidget>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _expandController;
+  late Animation<double> _animation;
+  bool _isDetailsDisplayed = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _prepareAnimations();
+  }
+
+  @override
+  void dispose() {
+    _expandController.dispose();
+    super.dispose();
+  }
+
+  void _prepareAnimations() {
+    _expandController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 500));
+    _animation = CurvedAnimation(
+      parent: _expandController,
+      curve: Curves.fastOutSlowIn,
+    );
+  }
+
+  void _evalDetailsPressed() {
+    _isDetailsDisplayed = !_isDetailsDisplayed;
+
+    if (_isDetailsDisplayed) {
+      _expandController.forward();
+    } else {
+      _expandController.reverse();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -25,6 +67,7 @@ class IncorrectMnemonicWidget extends StatelessWidget {
         ConstrainedBox(
           constraints: BoxConstraints(maxHeight: AppConfigs.size!.height * 0.8),
           child: ListView(
+            physics: ClampingScrollPhysics(),
             shrinkWrap: true,
             children: [
               Padding(
@@ -58,31 +101,44 @@ class IncorrectMnemonicWidget extends StatelessWidget {
                     ),
                     Padding(
                       padding: const EdgeInsets.symmetric(vertical: 28),
-                      child: Row(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(right: 6),
-                            child: Icon(
-                              Icons.arrow_downward,
-                              color: colorFFFFFF,
+                      child: GestureDetector(
+                        onTap: () => setState(() => _evalDetailsPressed()),
+                        child: Row(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(right: 6),
+                              child: Icon(
+                                Icons.arrow_downward,
+                                color: colorFFFFFF,
+                              ),
                             ),
+                            Text(
+                              'More details',
+                              style: tsS16W400CFF,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizeTransition(
+                      axisAlignment: 1.0,
+                      sizeFactor: _animation,
+                      child: Column(
+                        children: [
+                          Image.asset(
+                            'mnemonic_error.png'.asAssetImg(),
+                            fit: BoxFit.contain,
                           ),
-                          Text(
-                            'More details',
-                            style: tsS16W400CFF,
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 26),
+                            child: Text(
+                              'One or more of your 12-24 words are incorrect, make sure that the order is correct or if there is a typing error.',
+                              style: tsS18W400CFF,
+                            ),
                           ),
                         ],
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                        vertical: 26,
-                      ),
-                      child: Text(
-                        'One or more of your 12-24 words are incorrect, make sure that the order is correct or if there is a typing error.',
-                        style: tsS18W400CFF,
-                      ),
-                    ),
+                    )
                   ],
                 ),
               ),
