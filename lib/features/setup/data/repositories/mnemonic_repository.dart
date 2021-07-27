56c9924597ff34f647ac51d2851580ab89cb9981
Adriel Bomfim <adrielvbomfim@gmail.com>
2021-07-27 14:14:12 -0300

    fix: inserting missing implements

--- lib/features/setup/data/repositories/mnemonic_repository.dart
+++ lib/features/setup/data/repositories/mnemonic_repository.dart
@@ -1,13 +1,15 @@
 import 'package:dartz/dartz.dart';
 import 'package:polkadex/features/setup/data/datasources/mnemonic_remote_datasource.dart';
+import 'package:polkadex/features/setup/domain/repositories/imnemonic_repository.dart';
 
-class MnemonicRepository {
+class MnemonicRepository implements IMnemonicRepository {
   MnemonicRepository(
       {required MnemonicRemoteDatasource mnemonicRemoteDatasource})
       : _mnemonicRemoteDatasource = mnemonicRemoteDatasource;
 
   final MnemonicRemoteDatasource _mnemonicRemoteDatasource;
 
+  @override
   Future<Either<Error, List<String>>> generateMnemonic() async {
     final result = await _mnemonicRemoteDatasource.generateMnemonic();
 
