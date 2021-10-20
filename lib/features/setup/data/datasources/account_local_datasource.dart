46fd954495c783bced98825cd8ef0a4089572210
Adriel Bomfim <adriel@polkadex.trade>
2021-10-20 09:32:14 -0300

    fix: corrects the biomertric settings for keypair and password files

--- lib/features/setup/data/datasources/account_local_datasource.dart
+++ lib/features/setup/data/datasources/account_local_datasource.dart
@@ -4,15 +4,15 @@ class AccountLocalDatasource {
   Future<void> saveAccountStorage(String keypairJson,
       {String? password}) async {
     final BiometricStorageFile fileKeypair =
-        await BiometricStorage().getStorage('keypair');
+        await BiometricStorage().getStorage(
+      'keypair',
+      options: StorageFileInitOptions(authenticationRequired: false),
+    );
     await fileKeypair.write(keypairJson);
 
     if (password != null) {
       final BiometricStorageFile filePassword =
-          await BiometricStorage().getStorage(
-        'password',
-        options: StorageFileInitOptions(authenticationRequired: true),
-      );
+          await BiometricStorage().getStorage('password');
       await filePassword.write(password);
     }
 
