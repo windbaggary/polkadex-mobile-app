5cb26a2e4d0ef788be7d51a4572330a9d815fe90
Adriel Bomfim <adriel@polkadex.trade>
2021-10-29 11:16:12 -0300

    fix: removes duplicate loading popup call

--- lib/features/setup/presentation/screens/confirm_password_screen.dart
+++ lib/features/setup/presentation/screens/confirm_password_screen.dart
@@ -6,7 +6,6 @@ import 'package:polkadex/common/configs/app_config.dart';
 import 'package:polkadex/common/utils/colors.dart';
 import 'package:polkadex/common/utils/styles.dart';
 import 'package:polkadex/common/widgets/app_buttons.dart';
-import 'package:polkadex/common/widgets/loading_popup.dart';
 import 'package:polkadex/features/landing/screens/landing_screen.dart';
 import 'package:polkadex/features/setup/presentation/widgets/warning_mnemonic_widget.dart';
 import 'package:polkadex/features/setup/presentation/widgets/wallet_input_widget.dart';
@@ -46,137 +45,127 @@ class _ConfirmPasswordScreenState extends State<ConfirmPasswordScreen>
 
   @override
   Widget build(BuildContext context) {
-    return BlocListener<AccountCubit, AccountState>(
-      listener: (_, state) {
-        if (state is AccountPasswordValidating) {
-          LoadingPopup.show(
-            context: context,
-            text: 'We are almost there...',
-          );
-        }
-      },
-      child: Scaffold(
-        backgroundColor: color1C2023,
-        appBar: AppBar(
-          title: Text(
-            'Security Password',
-            style: tsS19W600CFF,
-          ),
-          leading: SizedBox(
-            height: 5,
-            child: AnimatedBuilder(
-              animation: _entryAnimation,
-              builder: (context, child) {
-                final value = _entryAnimation.value;
-                return Opacity(
-                  opacity: value,
-                  child: Transform(
-                      transform: Matrix4.identity()
-                        ..translate(-50 * (1.0 - value)),
-                      child: child),
-                );
-              },
-              child: AppBackButton(
-                onTap: () => _onPop(context),
-              ),
+    return Scaffold(
+      backgroundColor: color1C2023,
+      appBar: AppBar(
+        title: Text(
+          'Security Password',
+          style: tsS19W600CFF,
+        ),
+        leading: SizedBox(
+          height: 5,
+          child: AnimatedBuilder(
+            animation: _entryAnimation,
+            builder: (context, child) {
+              final value = _entryAnimation.value;
+              return Opacity(
+                opacity: value,
+                child: Transform(
+                    transform: Matrix4.identity()
+                      ..translate(-50 * (1.0 - value)),
+                    child: child),
+              );
+            },
+            child: AppBackButton(
+              onTap: () => _onPop(context),
             ),
           ),
-          elevation: 0,
         ),
-        body: CustomScrollView(
-          physics: ClampingScrollPhysics(),
-          slivers: [
-            SliverList(
-              delegate: SliverChildBuilderDelegate(
-                (BuildContext context, int index) {
-                  return Container(
-                    decoration: BoxDecoration(
-                      color: color2E303C,
-                      borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(20.0),
-                        bottomRight: Radius.circular(20.0),
+        elevation: 0,
+      ),
+      body: CustomScrollView(
+        physics: ClampingScrollPhysics(),
+        slivers: [
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return Container(
+                  decoration: BoxDecoration(
+                    color: color2E303C,
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(20.0),
+                      bottomRight: Radius.circular(20.0),
+                    ),
+                    boxShadow: <BoxShadow>[
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.10),
+                        blurRadius: 30,
+                        offset: Offset(0.0, 20.0),
                       ),
-                      boxShadow: <BoxShadow>[
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.10),
-                          blurRadius: 30,
-                          offset: Offset(0.0, 20.0),
+                    ],
+                  ),
+                  child: SafeArea(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(20),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              Text(
+                                'Login with security password',
+                                style: tsS26W600CFF,
+                              ),
+                              SizedBox(
+                                height: 16,
+                              ),
+                              Text(
+                                'Please enter your password.',
+                                style: tsS18W400CFF,
+                              ),
+                              SizedBox(
+                                height: 18,
+                              ),
+                              WalletInputWidget(
+                                title: 'Repeat Password',
+                                description: '',
+                                controller: _passwordController,
+                                obscureText: true,
+                                onChanged: (password) => setState(() =>
+                                    _isConfirmEnabled = password.length >= 8),
+                              ),
+                            ],
+                          ),
                         ),
                       ],
                     ),
-                    child: SafeArea(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(20),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.stretch,
-                              children: [
-                                Text(
-                                  'Login with security password',
-                                  style: tsS26W600CFF,
-                                ),
-                                SizedBox(
-                                  height: 16,
-                                ),
-                                Text(
-                                  'Please enter your password.',
-                                  style: tsS18W400CFF,
-                                ),
-                                SizedBox(
-                                  height: 18,
-                                ),
-                                WalletInputWidget(
-                                  title: 'Repeat Password',
-                                  description: '',
-                                  controller: _passwordController,
-                                  obscureText: true,
-                                  onChanged: (password) => setState(() =>
-                                      _isConfirmEnabled = password.length >= 8),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
+                  ),
+                );
+              },
+              childCount: 1,
+            ),
+          ),
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(20, 14, 20, 18),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  AppButton(
+                    label: 'Confirm',
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 12,
+                      vertical: 16,
                     ),
-                  );
-                },
-                childCount: 1,
+                    enabled: _isConfirmEnabled,
+                    onTap: () async {
+                      final isCorrect = await context
+                          .read<AccountCubit>()
+                          .confirmPassword(_passwordController.text);
+
+                      isCorrect
+                          ? _onNavigateToLanding(context)
+                          : _onShowIncorrectPasswordModal(context);
+                    },
+                  ),
+                ],
               ),
             ),
-            SliverFillRemaining(
-              hasScrollBody: false,
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(20, 14, 20, 18),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    AppButton(
-                      label: 'Confirm',
-                      padding: EdgeInsets.symmetric(
-                        horizontal: 12,
-                        vertical: 16,
-                      ),
-                      enabled: _isConfirmEnabled,
-                      onTap: () async {
-                        final isCorrect = await context
-                            .read<AccountCubit>()
-                            .confirmPassword(_passwordController.text);
-
-                        isCorrect
-                            ? _onNavigateToLanding(context)
-                            : _onShowIncorrectPasswordModal(context);
-                      },
-                    ),
-                  ],
-                ),
-              ),
-            )
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
