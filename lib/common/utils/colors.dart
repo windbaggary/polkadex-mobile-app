7d393495095cdde6fb4749323cb9b5fa7976a82f
Adriel Bomfim <adriel@polkadex.trade>
2021-11-03 10:15:28 -0300

    refactor: changes how colors are managed in the colors file

--- lib/common/utils/colors.dart
+++ lib/common/utils/colors.dart
@@ -4,19 +4,21 @@
 
 import 'package:flutter/material.dart';
 
-Color get color1C2023 => Color(0xFF1C2023);
-Color get color1C1C26 => Color(0xFF1C1C26);
-Color get colorABB2BC => Color(0xFFABB2BC);
-Color get colorFFFFFF => Color(0xFFFFFFFF);
-Color get colorE6007A => Color(0xFFE6007A);
-Color get color2E303C => Color(0xFF2E303C);
-Color get color8BA1BE => Color(0xFF8BA1BE);
-Color get color3B4150 => Color(0xFF3B4150);
-Color get color0CA564 => Color(0xFF0CA564);
-Color get colorF79326 => Color(0xFFF79326);
-Color get color24252C => Color(0xFF24252C);
-Color get colorD64CA8 => Color(0xFFD64CA8);
-Color get color1F1F1F => Color(0xFF1F1F1F);
-Color get color93949A => Color(0xFF93949A);
-Color get color8E8E93 => Color(0xFF8E8E93);
-Color get color02C076 => Color(0xFF02C076);
+abstract class AppColors {
+  static const Color color1C2023 = Color(0xFF1C2023);
+  static const Color color1C1C26 = Color(0xFF1C1C26);
+  static const Color colorABB2BC = Color(0xFFABB2BC);
+  static const Color colorFFFFFF = Color(0xFFFFFFFF);
+  static const Color colorE6007A = Color(0xFFE6007A);
+  static const Color color2E303C = Color(0xFF2E303C);
+  static const Color color8BA1BE = Color(0xFF8BA1BE);
+  static const Color color3B4150 = Color(0xFF3B4150);
+  static const Color color0CA564 = Color(0xFF0CA564);
+  static const Color colorF79326 = Color(0xFFF79326);
+  static const Color color24252C = Color(0xFF24252C);
+  static const Color colorD64CA8 = Color(0xFFD64CA8);
+  static const Color color1F1F1F = Color(0xFF1F1F1F);
+  static const Color color93949A = Color(0xFF93949A);
+  static const Color color8E8E93 = Color(0xFF8E8E93);
+  static const Color color02C076 = Color(0xFF02C076);
+}
