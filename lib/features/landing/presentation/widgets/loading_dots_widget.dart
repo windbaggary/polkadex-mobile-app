36698ca0439ac4ad11f5a1c88826a620c4242fef
Adriel Bomfim <adriel@polkadex.trade>
2021-11-10 12:04:43 -0300

    feat: adds loading dots widget to project

---
+++ lib/features/landing/presentation/widgets/loading_dots_widget.dart
@@ -0,0 +1,58 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class LoadingDotsWidget extends StatefulWidget {
+  const LoadingDotsWidget({required this.dotSize});
+
+  final double dotSize;
+
+  @override
+  State<LoadingDotsWidget> createState() => _LoadingDotsWidgetState();
+}
+
+class _LoadingDotsWidgetState extends State<LoadingDotsWidget> {
+  int indexDotFade = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance?.addPostFrameCallback(
+      (_) => Timer.periodic(
+        Duration(milliseconds: 500),
+        (_) => setState(() => indexDotFade = (indexDotFade + 1) % 3),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: List.generate(
+        3,
+        (index) => Container(
+          width: widget.dotSize,
+          height: widget.dotSize,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: index == indexDotFade
+                ? Colors.white.withOpacity(0.5)
+                : Colors.white,
+          ),
+        ),
+      )
+        ..insert(
+            1,
+            SizedBox(
+              width: 4,
+            ))
+        ..insert(
+            3,
+            SizedBox(
+              width: 4,
+            )),
+    );
+  }
+}
