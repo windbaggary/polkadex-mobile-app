14a67f76d266f47a022ebfa43e8faa2872350ab3
Adriel Bomfim <adriel@polkadex.trade>
2021-12-20 13:37:38 -0300

    fix: sets filterTouchesWhenObscured to true for the android app

--- android/app/src/main/kotlin/com/example/polkadex/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/polkadex/MainActivity.kt
@@ -1,4 +1,12 @@
 package com.example.polkadex
+import android.view.View;
 import io.flutter.embedding.android.FlutterFragmentActivity;
 
-class MainActivity: FlutterFragmentActivity() {}
+class MainActivity: FlutterFragmentActivity() {
+    override fun onStart() {
+        super.onStart()
+
+        val view = findViewById<View>(android.R.id.content).rootView
+        view.filterTouchesWhenObscured = true
+    }
+}
