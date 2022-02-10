8620275da14f7c87aa2fe23b684e6b1659c693ce
Adriel Bomfim <adriel@polkadex.trade>
2022-02-10 15:22:11 -0300

    feat: integrates graph data to graph widget from kchart library

--- lib/common/graph/domain/repositories/igraph_repository.dart
+++ lib/common/graph/domain/repositories/igraph_repository.dart
@@ -1,5 +1,4 @@
 import 'package:dartz/dartz.dart';
-import 'package:polkadex/common/graph/domain/entities/line_chart_entity.dart';
 import 'package:polkadex/common/network/error.dart';
 import 'package:polkadex/common/utils/enums.dart';
 import 'package:k_chart/entity/k_line_entity.dart';
