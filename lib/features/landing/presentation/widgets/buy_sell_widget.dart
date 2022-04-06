7e973404c5ef77d711297279a50fd1ece83ddbfb
Adriel Venancio Bomfim <adriel@polkadex.trade>
2022-04-06 13:21:33 -0300

    feat: improves buy and sell buttons

--- lib/features/landing/presentation/widgets/buy_sell_widget.dart
+++ lib/features/landing/presentation/widgets/buy_sell_widget.dart
@@ -1,21 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:polkadex/common/configs/app_config.dart';
 import 'package:polkadex/common/utils/colors.dart';
+import 'package:polkadex/common/utils/extensions.dart';
 import 'package:polkadex/common/utils/styles.dart';
+import 'package:polkadex/common/utils/enums.dart';
 
 class BuySellWidget extends StatelessWidget {
+  const BuySellWidget({required this.buySellNotifier});
+
+  final ValueNotifier<EnumBuySell> buySellNotifier;
+
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [_orderSideButton(), _orderSideButton(isBuy: false)],
+    return ValueListenableBuilder<EnumBuySell>(
+      valueListenable: buySellNotifier,
+      builder: (context, buySellValue, _) => Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          GestureDetector(
+            onTapDown: (_) => buySellNotifier.value = EnumBuySell.buy,
+            child: _orderSideButton(isActive: buySellValue == EnumBuySell.buy),
+          ),
+          GestureDetector(
+            onTapDown: (_) => buySellNotifier.value = EnumBuySell.sell,
+            child: _orderSideButton(
+              isActive: buySellValue == EnumBuySell.sell,
+              isBuy: false,
+            ),
+          )
+        ],
+      ),
     );
   }
 
-  Widget _orderSideButton({bool isBuy = true}) {
+  Widget _orderSideButton({
+    bool isActive = false,
+    bool isBuy = true,
+  }) {
     return Container(
       decoration: BoxDecoration(
-        color: AppColors.color3B4150,
+        color: isActive
+            ? (isBuy ? AppColors.color0CA564 : AppColors.colorE6007A)
+            : AppColors.color3B4150,
         borderRadius: BorderRadius.all(Radius.circular(10)),
       ),
       padding: EdgeInsets.symmetric(vertical: 20),
@@ -23,13 +50,33 @@ class BuySellWidget extends StatelessWidget {
       child: Align(
         alignment: Alignment.center,
         child: isBuy
-            ? Text(
-                'Buy',
-                style: tsS13W600CFF,
+            ? Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SvgPicture.asset(
+                    'buy_circle'.asAssetSvg(),
+                    height: 16,
+                  ),
+                  SizedBox(width: 4),
+                  Text(
+                    'Buy',
+                    style: tsS16W600CFF,
+                  ),
+                ],
               )
-            : Text(
-                'Sell',
-                style: tsS13W600CFF,
+            : Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SvgPicture.asset(
+                    'sell_circle'.asAssetSvg(),
+                    height: 16,
+                  ),
+                  SizedBox(width: 4),
+                  Text(
+                    'Sell',
+                    style: tsS16W600CFF,
+                  ),
+                ],
               ),
       ),
     );
--- lib/features/landing/presentation/widgets/place_order_widget.dart
+++ lib/features/landing/presentation/widgets/place_order_widget.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:polkadex/features/landing/presentation/widgets/buy_sell_widget.dart';
+import 'package:polkadex/common/utils/enums.dart';
 
-class PlaceOrderWidget extends StatelessWidget {
+class PlaceOrderWidget extends StatefulWidget {
+  @override
+  State<PlaceOrderWidget> createState() => _PlaceOrderWidgetState();
+}
+
+class _PlaceOrderWidgetState extends State<PlaceOrderWidget> {
+  final ValueNotifier<EnumBuySell> _buySellNotifier =
+      ValueNotifier(EnumBuySell.buy);
+
   @override
   Widget build(BuildContext context) {
-    return BuySellWidget();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        BuySellWidget(
+          buySellNotifier: _buySellNotifier,
+        ),
+      ],
+    );
   }
 }
