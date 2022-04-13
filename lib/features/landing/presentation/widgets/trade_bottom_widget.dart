9389727d843366b272b7327d4f0c96ab573ec1ad
Adriel Venancio Bomfim <adriel@polkadex.trade>
2022-04-13 12:26:49 -0300

    fix: disables trade history and funds buttons for now

--- lib/features/landing/presentation/widgets/trade_bottom_widget.dart
+++ lib/features/landing/presentation/widgets/trade_bottom_widget.dart
@@ -41,11 +41,14 @@ class TradeBottomWidget extends StatelessWidget {
                   },
                 ),
                 _displayOptionWidget(
-                    text: 'Trade History',
-                    enumValue: EnumTradeBottomDisplayTypes.tradeHistory),
+                  text: 'Trade History',
+                  enumValue: EnumTradeBottomDisplayTypes.tradeHistory,
+                  enabled: false,
+                ),
                 _displayOptionWidget(
                   text: 'Funds',
                   enumValue: EnumTradeBottomDisplayTypes.funds,
+                  enabled: false,
                 ),
                 SvgPicture.asset(
                   'expand'.asAssetSvg(),
@@ -105,35 +108,41 @@ class TradeBottomWidget extends StatelessWidget {
   Widget _displayOptionWidget({
     required String text,
     required EnumTradeBottomDisplayTypes enumValue,
+    bool enabled = true,
   }) {
     return GestureDetector(
-      onTapDown: (_) => tradeBottomDisplayNotifier.value = enumValue,
-      child: Container(
-        decoration: BoxDecoration(
-          color: tradeBottomDisplayNotifier.value == enumValue
-              ? Colors.black
-              : Colors.white,
-          borderRadius: BorderRadius.all(Radius.circular(10)),
-        ),
-        padding: EdgeInsets.all(16),
-        child: Row(
-          children: [
-            if (enumValue == EnumTradeBottomDisplayTypes.orderHistory)
-              SvgPicture.asset(
-                'orders'.asAssetSvg(),
-                width: 14,
-                color: tradeBottomDisplayNotifier.value == enumValue
-                    ? Colors.white
-                    : AppColors.color141415,
+      onTapDown: enabled
+          ? (_) => tradeBottomDisplayNotifier.value = enumValue
+          : (_) {},
+      child: Opacity(
+        opacity: enabled ? 1.0 : 0.3,
+        child: Container(
+          decoration: BoxDecoration(
+            color: tradeBottomDisplayNotifier.value == enumValue
+                ? Colors.black
+                : Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+          ),
+          padding: EdgeInsets.all(16),
+          child: Row(
+            children: [
+              if (enumValue == EnumTradeBottomDisplayTypes.orderHistory)
+                SvgPicture.asset(
+                  'orders'.asAssetSvg(),
+                  width: 14,
+                  color: tradeBottomDisplayNotifier.value == enumValue
+                      ? Colors.white
+                      : AppColors.color141415,
+                ),
+              SizedBox(width: 6),
+              Text(
+                text,
+                style: tradeBottomDisplayNotifier.value == enumValue
+                    ? tsS16W600CFF
+                    : tsS16W600C141415,
               ),
-            SizedBox(width: 6),
-            Text(
-              text,
-              style: tradeBottomDisplayNotifier.value == enumValue
-                  ? tsS16W600CFF
-                  : tsS16W600C141415,
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
