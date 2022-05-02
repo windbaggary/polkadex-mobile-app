2cedbec536ffd6cff15a1432fe1c23a91989067c
Adriel Venancio Bomfim <adriel@polkadex.trade>
2022-05-02 13:22:16 -0300

    fix: fixes response from sendRpcRequest method

--- lib/common/network/blockchain_rpc_helper.dart
+++ lib/common/network/blockchain_rpc_helper.dart
@@ -4,15 +4,18 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
 abstract class BlockchainRpcHelper {
-  static Future<void> sendRpcRequest(String methodName, dynamic payload) async {
+  static Future<dynamic> sendRpcRequest(
+      String methodName, dynamic payload) async {
     try {
-      var socket = WebSocketChannel.connect(Uri.parse(dotenv.get('BLOCKCHAIN_URL')));
+      var socket =
+          WebSocketChannel.connect(Uri.parse(dotenv.get('BLOCKCHAIN_URL')));
       var client = Client(socket.cast<String>());
       client.listen();
 
-      final result = await client
-          .sendRequest(methodName, payload);
+      final result = await client.sendRequest(methodName, payload);
       client.close();
+
+      return result;
     } catch (e) {
       print(e);
     }
