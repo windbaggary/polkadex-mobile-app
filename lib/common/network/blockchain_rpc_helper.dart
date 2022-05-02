11246fb778949e964954ba1d140f93c90ce21afe
Adriel Venancio Bomfim <adriel@polkadex.trade>
2022-05-02 13:10:07 -0300

    feat: creates class and method for rpc call

---
+++ lib/common/network/blockchain_rpc_helper.dart
@@ -0,0 +1,20 @@
+import "dart:async";
+import 'package:json_rpc_2/json_rpc_2.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
+
+abstract class BlockchainRpcHelper {
+  static Future<void> sendRpcRequest(String methodName, dynamic payload) async {
+    try {
+      var socket = WebSocketChannel.connect(Uri.parse(dotenv.get('BLOCKCHAIN_URL')));
+      var client = Client(socket.cast<String>());
+      client.listen();
+
+      final result = await client
+          .sendRequest(methodName, payload);
+      client.close();
+    } catch (e) {
+      print(e);
+    }
+  }
+}
