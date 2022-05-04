c0d89ffd3b165659a07a7275c9716b772b40aa17
Adriel Venancio Bomfim <adriel@polkadex.trade>
2022-05-04 13:07:16 -0300

    feat: db config using dotenv secrets

--- lib/common/network/mysql_client.dart
+++ lib/common/network/mysql_client.dart
@@ -1,18 +1,19 @@
 import "dart:async";
 import 'package:mysql1/mysql1.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class MysqlClient {
   final settings = ConnectionSettings(
-    host: '',
-    port: 0000,
-    user: '',
-    password: '',
-    db: '',
+      host: dotenv.get('DB_HOST'),
+      port: int.parse(dotenv.get('DB_PORT')),
+      user: dotenv.get('DB_USER'),
+      password: dotenv.get('DB_PASSWORD'),
+      db: dotenv.get('DB_NAME'),
   );
 
   Future<dynamic> connectToDb() async {
     try {
-      await MySqlConnection.connect(settings);
+      final conn = await MySqlConnection.connect(settings);
     } catch (e) {
       print(e);
     }
