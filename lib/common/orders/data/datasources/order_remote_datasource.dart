de5883c256edcd62155131e6d8460af4bf3930ad
Adriel Venancio Bomfim <adriel@polkadex.trade>
2022-05-16 17:54:07 -0300

    fix: inserts try catch block for place order

--- lib/common/orders/data/datasources/order_remote_datasource.dart
+++ lib/common/orders/data/datasources/order_remote_datasource.dart
@@ -20,31 +20,37 @@ class OrderRemoteDatasource {
     String address,
     String signature,
   ) async {
-    final dbClient = dependency<MysqlClient>();
-    await dbClient.init();
+    try {
+      final dbClient = dependency<MysqlClient>();
+      await dbClient.init();
 
-    final dbProxyResult = await dbClient.conn
-        .execute("select * from proxies where proxy = :proxyAddress", {
-      "proxyAddress": address,
-    });
-    final dbAccId = dbProxyResult.rows.first.colByName('id');
+      final dbProxyResult = await dbClient.conn
+          .execute("select * from proxies where proxy = :proxyAddress", {
+        "proxyAddress": address,
+      });
+      final dbAccId = dbProxyResult.rows.first.colByName('id');
 
-    final dbMainResult = await dbClient.conn
-        .execute("select * from accounts where id = :acc_id", {
-      "acc_id": dbAccId,
-    });
-    final dbMainAddress = dbMainResult.rows.first.colByName('main_acc');
+      final dbMainResult = await dbClient.conn
+          .execute("select * from accounts where id = :acc_id", {
+        "acc_id": dbAccId,
+      });
+      final dbMainAddress = dbMainResult.rows.first.colByName('main_acc');
 
-    final nonce = await BlockchainRpcHelper.sendRpcRequest(
-        'enclave_getNonce', [dbMainAddress]);
+      final nonce = await BlockchainRpcHelper.sendRpcRequest(
+          'enclave_getNonce', [dbMainAddress]);
 
-    final String _callPlaceOrderJSON =
-        "polkadexWorker.placeOrderJSON(keyring.getPair('$address'), $nonce, '$baseAsset', '$quoteAsset', '$orderType', '$orderSide', $price, $amount)";
-    final List<dynamic> payloadResult = await dependency<WebViewRunner>()
-        .evalJavascript(_callPlaceOrderJSON, isSynchronous: true);
+      final String _callPlaceOrderJSON =
+          "polkadexWorker.placeOrderJSON(keyring.getPair('$address'), $nonce, '$baseAsset', '$quoteAsset', '$orderType', '$orderSide', $price, $amount)";
+      final List<dynamic> payloadResult = await dependency<WebViewRunner>()
+          .evalJavascript(_callPlaceOrderJSON, isSynchronous: true);
 
-    return (BlockchainRpcHelper.sendRpcRequest(
-        'enclave_placeOrder', payloadResult) as double?);
+      return (BlockchainRpcHelper.sendRpcRequest(
+          'enclave_placeOrder', payloadResult) as double?);
+    }
+    catch (e) {
+      print(e);
+      return null;
+    }
   }
 
   Future<Response> cancelOrder(
