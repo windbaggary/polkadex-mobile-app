b8ec711cf5b419ff01139bbb872841923c145acd
Adriel Venancio Bomfim <adriel@polkadex.trade>
2022-05-20 10:30:22 -0300

    feat: adds order history in db client method

--- lib/common/network/mysql_client.dart
+++ lib/common/network/mysql_client.dart
@@ -13,6 +13,30 @@ class MysqlClient {
       password: dotenv.get('DB_PASSWORD'),
       databaseName: dotenv.get('DB_NAME'),
     );
-    await conn.connect();
+
+    if (!conn.connected) {
+      await conn.connect();
+    }
+  }
+
+  Future<String?> getAccountId(String proxyAddress) async {
+    await init();
+
+    final dbProxyResult =
+        await conn.execute("select * from proxies where :proxyAddress", {
+      "proxyAddress": proxyAddress,
+    });
+    return dbProxyResult.rows.first.colByName('id');
+  }
+
+  Future<IResultSet> getOrderHistory(String proxyAddress) async {
+    final mainAccId = await getAccountId(proxyAddress);
+
+    return await conn.execute(
+      "select txid , base_asset_type , quote_asset_type, order_type, status, qty, price from transactions where main_account_id = :main_account_id",
+      {
+        "main_account_id": mainAccId,
+      },
+    );
   }
 }
