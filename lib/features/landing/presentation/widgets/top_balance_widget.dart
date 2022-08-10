934440bffd2a6c7a23e81819e20034f55622fbb9
Adriel Venancio Bomfim <adriel@polkadex.trade>
2022-08-10 13:30:51 -0300

    feat: adds proxy address and button to copy it

--- lib/features/landing/presentation/widgets/top_balance_widget.dart
+++ lib/features/landing/presentation/widgets/top_balance_widget.dart
@@ -1,9 +1,12 @@
+import 'package:clipboard/clipboard.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:polkadex/common/configs/app_config.dart';
 import 'package:polkadex/common/utils/colors.dart';
 import 'package:polkadex/common/utils/extensions.dart';
 import 'package:polkadex/common/utils/styles.dart';
+import 'package:polkadex/common/widgets/build_methods.dart';
 import 'package:polkadex/common/cubits/account_cubit/account_cubit.dart';
 import 'package:polkadex/features/landing/presentation/cubits/balance_cubit/balance_cubit.dart';
 
@@ -12,28 +15,61 @@ class TopBalanceWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<BalanceCubit, BalanceState>(
       builder: (context, state) {
-        return Column(
-          children: [
-            Center(
-              child: Container(
-                width: 42,
-                height: 42,
-                margin: const EdgeInsets.only(bottom: 9),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                  color: AppColors.color8BA1BE.withOpacity(0.30),
+        return Center(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(maxWidth: AppConfigs.size.width / 3),
+            child: Column(
+              children: [
+                Center(
+                  child: Container(
+                    width: 42,
+                    height: 42,
+                    margin: const EdgeInsets.only(bottom: 16),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      color: AppColors.color8BA1BE.withOpacity(0.30),
+                    ),
+                    padding: const EdgeInsets.all(11),
+                    child: SvgPicture.asset('wallet_selected'.asAssetSvg()),
+                  ),
                 ),
-                padding: const EdgeInsets.all(11),
-                child: SvgPicture.asset('wallet_selected'.asAssetSvg()),
-              ),
+                Text(
+                  context.read<AccountCubit>().accountName,
+                  style: tsS20W600CFF,
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 8),
+                Row(
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        FlutterClipboard.copy(context
+                                .read<AccountCubit>()
+                                .proxyAccountAddress)
+                            .then((value) => buildAppToast(
+                                msg:
+                                    'Wallet address has been copied to the clipboard',
+                                context: context));
+                      },
+                      child: SvgPicture.asset(
+                        'copy'.asAssetSvg(),
+                      ),
+                    ),
+                    SizedBox(width: 2),
+                    Flexible(
+                      child: Text(
+                        context.read<AccountCubit>().proxyAccountAddress,
+                        overflow: TextOverflow.ellipsis,
+                        style:
+                            tsS14W400CFF.copyWith(color: AppColors.colorABB2BC),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
-            Text(
-              context.read<AccountCubit>().accountName,
-              style: tsS20W600CFF,
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(height: 16),
-          ],
+          ),
         );
       },
     );
