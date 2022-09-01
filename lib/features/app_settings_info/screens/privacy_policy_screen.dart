a381fc4b2cd322e2fc9243a2cdbc83fc4cf0538c
Adriel Venancio Bomfim <adriel@polkadex.trade>
2022-09-01 09:28:27 -0300

    feat: fetchs both local and remote pdfs and shows the first one if the remote fails to be fetched

--- lib/features/app_settings_info/screens/privacy_policy_screen.dart
+++ lib/features/app_settings_info/screens/privacy_policy_screen.dart
@@ -1,7 +1,6 @@
 import 'package:advance_pdf_viewer/advance_pdf_viewer.dart';
 import 'package:flutter/material.dart';
 import 'package:polkadex/common/widgets/loading_dots_widget.dart';
-import 'package:polkadex/common/widgets/polkadex_progress_error_widget.dart';
 import 'package:polkadex/features/app_settings_info/widgets/app_settings_layout.dart';
 import 'package:polkadex/common/utils/colors.dart';
 
@@ -12,11 +11,11 @@ class PrivacyPolicyScreen extends StatefulWidget {
 }
 
 class _PrivacyPolicyScreenState extends State<PrivacyPolicyScreen> {
-  final ValueNotifier<PDFDocument?> marketDropDownNotifier =
-      ValueNotifier<PDFDocument?>(null);
+  late PDFDocument localPdf;
 
-  Future<PDFDocument> get _pdfRequest => PDFDocument.fromURL(
-      'https://github.com/Polkadex-Substrate/Docs/raw/master/Polkadex_Privacy_Policy.pdf');
+  final String _pdfLink =
+      'https://github.com/Polkadex-Substrate/Docs/raw/master/Polkadex_Privacy_Policy.pdf';
+  final String _pdfLocalPath = 'assets/pdfs/Polkadex_Privacy_Policy.pdf';
 
   @override
   Widget build(BuildContext context) {
@@ -39,7 +38,10 @@ class _PrivacyPolicyScreenState extends State<PrivacyPolicyScreen> {
             ),
           ),
           child: FutureBuilder(
-            future: _pdfRequest,
+            future: _fetchPdfs(
+              _pdfLink,
+              _pdfLocalPath,
+            ),
             builder:
                 (BuildContext context, AsyncSnapshot<PDFDocument> snapshot) {
               if (snapshot.connectionState == ConnectionState.waiting) {
@@ -49,26 +51,20 @@ class _PrivacyPolicyScreenState extends State<PrivacyPolicyScreen> {
                   ),
                 );
               } else {
-                if (snapshot.hasData) {
-                  return Theme(
-                    // ignore accentColor deprecation since it's the only way to set desired pdf page on numberPicker
-                    // ignore_for_file: deprecated_member_use
-                    data: currentTheme.copyWith(
-                      accentColor: AppColors.colorE6007A,
-                      colorScheme: currentTheme.colorScheme.copyWith(
-                        primary: AppColors.colorE6007A,
-                      ),
-                    ),
-                    child: PDFViewer(
-                      document: snapshot.data!,
-                      pickerButtonColor: AppColors.colorE6007A,
-                    ),
-                  );
-                }
+                final pdfDocument = snapshot.hasData ? snapshot.data : localPdf;
 
-                return Center(
-                  child: PolkadexErrorRefreshWidget(
-                    onRefresh: () async => setState(() {}),
+                return Theme(
+                  // ignore accentColor deprecation since it's the only way to set desired pdf page on numberPicker
+                  // ignore_for_file: deprecated_member_use
+                  data: currentTheme.copyWith(
+                    accentColor: AppColors.colorE6007A,
+                    colorScheme: currentTheme.colorScheme.copyWith(
+                      primary: AppColors.colorE6007A,
+                    ),
+                  ),
+                  child: PDFViewer(
+                    document: pdfDocument!,
+                    pickerButtonColor: AppColors.colorE6007A,
                   ),
                 );
               }
@@ -78,4 +74,19 @@ class _PrivacyPolicyScreenState extends State<PrivacyPolicyScreen> {
       ),
     );
   }
+
+  Future<PDFDocument> _fetchPdfs(
+    String link,
+    String path,
+  ) async {
+    localPdf = await _pdfRequestFromAsset(path);
+
+    return _pdfRequestFromURL(link);
+  }
+
+  Future<PDFDocument> _pdfRequestFromAsset(String path) =>
+      PDFDocument.fromAsset(path);
+
+  Future<PDFDocument> _pdfRequestFromURL(String link) =>
+      PDFDocument.fromURL(link);
 }
